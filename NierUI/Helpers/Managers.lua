2d4af572c5eb6a3e5020f708f44ca1569f297183
Mikhail Supov <supchyan@yandex.ru>
2025-05-14 14:10:51 +0300

    Delete NierUI/Helpers/Managers.lua

--- NierUI/Helpers/Managers.lua
+++
@@ -1,8 +0,0 @@
-local Managers = {}
-
-Managers.Network    = sdk.get_managed_singleton("app.NetworkManager")
-Managers.Player     = sdk.get_managed_singleton("app.PlayerManager")
-Managers.Enemy      = sdk.get_managed_singleton("app.EnemyManager")
-Managers.GUI        = sdk.get_managed_singleton("app.GUIManager")
-
-return Managers
